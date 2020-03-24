206e78c72b70ec06fe0ad3c09963d63d303d542f
MridulChauhan <mridulchauhan1998@gmail.com>
2020-03-24 17:24:12 +0530

    Add files via upload

---
+++ Arduino Codes/Receiver/Receiver.ino
@@ -0,0 +1,46 @@
+// Receiver NODE02 (B)
+
+#include <RF24.h>
+#include <RF24Network.h>
+#include <SPI.h>
+RF24 radio(7,8);               // nRF24L01 (CE,CSN)
+RF24Network network(radio);      // Include the radio in the network
+char this_node='B';
+const uint16_t node02 = 02; // Address of our node in Octal format ( 04,031, etc)
+char address;  
+
+void setup() 
+{
+  Serial.begin(9600);
+  SPI.begin();
+  radio.begin();
+  network.begin(90, node02); //(channel, node address)
+  radio.setDataRate(RF24_2MBPS);
+}
+void loop() 
+{
+  network.update();
+  while ( network.available() )  // Is there any incoming data?
+  {    
+    RF24NetworkHeader header5;
+    network.read(header5, &address, sizeof(address)); // Read the incoming data
+    Serial.println(address);
+    delay(2000);
+    /*RF24NetworkHeader header;
+    unsigned long temp;
+    unsigned long h;
+    unsigned long smoke;
+    network.read(header, &temp, sizeof(temp)); // Read the incoming data
+    Serial.print("Temp : ");
+    Serial.println(temp);
+    delay(3000);
+    network.read(header, &smoke, sizeof(smoke)); // Read the incoming data
+    Serial.print("Smoke : ");
+    Serial.println(smoke);
+    delay(3000);
+    network.read(header, &h, sizeof(h)); // Read the incoming data
+    Serial.print("Humidity : ");
+    Serial.println(h);
+    delay(3000);*/
+  }
+}
---
+++ Arduino Codes/Transmitter/Transmitter.ino
@@ -0,0 +1,55 @@
+// Transmitter NODE00 (A)
+
+#include <RF24.h>
+#include <RF24Network.h>
+#include <SPI.h>
+#include "DHT.h"
+#define DHTPIN A0
+#define DHTTYPE DHT11 
+DHT dht(DHTPIN, DHTTYPE); 
+RF24 radio(7,8);               // nRF24L01 (CE,CSN)
+RF24Network network(radio);      // Include the radio in the network
+char this_node='A';   // Address of this node in Octal format ( 04,031, etc)
+const uint16_t node00 = 00;
+const uint16_t node02 = 02;
+const uint16_t node03 = 03;
+  
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+  SPI.begin();
+  radio.begin();
+  network.begin(90, node00); //(channel, node address)
+  radio.setDataRate(RF24_2MBPS);
+}
+void loop() 
+{
+  network.update();
+    //float f = dht.readTemperature(true);
+    unsigned long h = dht.readHumidity();  // Read the temp value
+    unsigned long temp = dht.readTemperature();
+    unsigned long smoke=analogRead(A1);
+    // Compute heat index
+    // Must send in temp in Fahrenheit!
+    //float hi = dht.computeHeatIndex(f, h);
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(temp);
+    Serial.print(" *C ");
+    //Serial.print("Heat index: ");
+    //Serial.print(hi);
+    //Serial.println(" *F");
+    Serial.print("Smoke : ");
+    Serial.println(smoke); 
+    if(temp>25 && smoke>150)
+    {
+      RF24NetworkHeader header5(node03);
+      bool ok3 = network.write(header5, &this_node, sizeof(this_node));
+      Serial.println(this_node); 
+      delay(2000);
+    }
+}
+  
